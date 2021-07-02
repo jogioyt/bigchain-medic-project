34bcc58a7754771ad96a96b9e4dac1f6695360ad
babboonz <73415424+babboonz@users.noreply.github.com>
2021-07-02 22:34:06 +0700

    Add files via upload

---
+++ try_rbac_log.py
@@ -0,0 +1,45 @@
+#Tribe 1 details:
+Tribe1 = {
+    'asset': {
+        'data': {
+            'data': {
+                'link': 'bfc131382f579aaf29107c26dcafb1e1e2d096257126870df1f0a4b28004fa49', 
+                'name': 'tribe1', 
+                'ns': 'rbac-bdb-demo.tribe1'
+            }, 
+            'link': 'bfc131382f579aaf29107c26dcafb1e1e2d096257126870df1f0a4b28004fa49', 
+            'name': 'tribe1', 
+            'ns': 'rbac-bdb-demo.tribe1'
+        }
+    }, 
+    'id': '1e2292e507568bba46dd7ae193f68b6ceb9d24fc301fc79577287f54254ec6fb', 
+    'inputs': [
+        {
+            'fulfillment': 'pGSAIDqXf6H35SCWdMsNCYOJ2LsmGRCt3AariQZ-H93kSSZUgUBa4AxqeHmTL2vR1zyOXRfG15UsEVmp42XVeFLz1bBZNOV8J3hTpdTysgpgvugypWmqg08335MUL-qqCs7HkCID', 
+            'fulfills': None, 
+            'owners_before': [
+                '4widgxqwn3mFC7vxS7cCp7d3dPGqZQSPhdwqGr5Njw87'
+            ]
+        }
+    ],
+    'metadata': {
+        'can_link': '806af584b35025fbb47b2f76b43b336224fbf0874ec41830f623485a3876940d'
+    }, 
+    'operation': 'CREATE', 
+    'outputs': [
+        {
+            'amount': '1', 
+            'condition': {
+                'details': {
+                    'public_key': '4widgxqwn3mFC7vxS7cCp7d3dPGqZQSPhdwqGr5Njw87', 
+                    'type': 'ed25519-sha-256'
+                }, 
+                'uri': 'ni:///sha-256;8G4K2YbLIysYuV60IvzBeeWOfw_R6fYz1HxS9gFNCa0?fpt=ed25519-sha-256&cost=131072'
+            }, 
+            'public_keys': [
+                '4widgxqwn3mFC7vxS7cCp7d3dPGqZQSPhdwqGr5Njw87'
+            ]
+        }
+    ], 
+    'version': '2.0'
+}
---
+++ try_rbac_methods.py
@@ -0,0 +1,94 @@
+#import the necessary modules which
+from bigchaindb_driver import *
+from datetime import datetime
+from bdb_transaction import create_transaction, transfer_transaction
+import asyncio
+
+#initiate bigchaindb
+bdb_root_url = 'http://localhost:9984/'
+bdb = BigchainDB(bdb_root_url)
+
+#function to create identifier for the app
+def createGroup(namespace, admin_keys):
+    ns = namespace+'.admin'
+    adminGroupAsset = {
+        'ns': ns,
+        'name' : 'admin',
+        'data':{
+            'ns':ns,
+            'name': 'admin'
+        }
+    }
+    adminGroupMetadata = {
+        'can_link' : admin_keys['public_key']
+    }
+    adminGroup = create_transaction(admin_keys, adminGroupAsset, adminGroupMetadata)
+    return adminGroup
+
+def createApp(namespace, admin_keys, adminGroupId):
+    appAsset = {
+        'ns' : namespace,
+        'name' : namespace,
+        'data': {
+            'ns' : namespace,
+            'name' : namespace
+        }
+    }
+    appMetadata = {
+        'can_link' : adminGroupId
+    }
+    app_tx = create_transaction(admin_keys, appAsset, appMetadata)
+    return app_tx
+
+#function createUser
+def createUser(namespace, adminKeyPair, userTypeId, userTypeName, userPublicKey):
+    ns_asset = namespace+"."+userTypeName
+    asset = {
+        'ns': ns_asset,
+        'link' : userTypeId,
+        'createdBy': adminKeyPair["public_key"],
+        'type' : userTypeName,
+        'keyword':'UserAsset'
+    }
+    date = datetime.now()
+    timestamp = date.strftime("%d/%m/%Y %H:%M:%S")
+    metadata = {
+        'event':'User Assigned',
+        'timestamp': timestamp,
+        'publicKey' : adminKeyPair["public_key"],
+        'eventData':{
+            'userType':userTypeName
+        }
+    }
+    instanceTx = create_transaction(adminKeyPair, asset, metadata)
+    transfer_transaction(instanceTx, adminKeyPair,userPublicKey, metadata)
+    return instanceTx
+
+#function create type
+def createType(adminKeys, namespace, typeName, appId, canLinkAssetId):
+    ns_asset = namespace+"."+typeName
+    asset = {
+        'ns': ns_asset,
+        'link': appId,
+        'name' : typeName,
+        'data':{
+            'ns': ns_asset,
+            'link': appId,
+            'name': typeName
+        }
+    }
+    metadata = {
+        'can_link': canLinkAssetId
+    }
+    typeTx = create_transaction(adminKeys, asset, metadata)
+    return typeTx
+
+#function create type instance that are being used setting an user to become either "proposal" or "vote"
+def createTypeInstance(namespace, keypair, typeName, typeId, metadata):
+    ns_asset = namespace+"."+typeName
+    asset = {
+        'ns' : ns_asset,
+        'link' : typeId
+    }
+    tx = create_transaction(keypair, asset, metadata)
+    return tx
