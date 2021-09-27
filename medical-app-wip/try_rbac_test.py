0916a0e360f6a225b982ca52bfabcf79b2eb3903
babboonz <73415424+babboonz@users.noreply.github.com>
2021-09-27 11:47:13 +0700

    Delete try_rbac_test.py

--- medical-app-wip/try_rbac_test.py
+++
@@ -1,8 +0,0 @@
-#import the necessary modules which
-from bigchaindb_driver import *
-from bigchaindb_driver.common.transaction import Transaction
-from bigchaindb_driver.crypto import generate_keypair
-from bdb_transaction import *
-from datetime import datetime
-from try_rbac_methods import *
-
